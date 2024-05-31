8a7f638ab7df205c887af9109b711913d2ab7695
Alumno <alumno@isbo.edu.uy>
2024-05-31 20:51:59 -0300

    modificamos form.php

--- form.php
+++ form.php
@@ -1,9 +1,11 @@
-<?php
-
-function agregarUsuario($arrayusuario,$nombre,$edad,$email){
-$arrayusuarios = array(
-    "nombre" => $nombre,
-    "edad" => $edad,
-    "email" => $email
-);}
-echo agregarUsuario($arrayusuario);
+<?
+function agregarUsuario($arrayusuarios,$nombre,$edad,$email){
+    $arrayusuarios = array(
+        "nombre" => $nombre,
+        "edad" => $edad,
+        "email" => $email
+    );
+}
+$arrayusuarios = [];
+$arrayusuarios= agregarUsuario($arrayusuarios,"Juan","24","juanperez@gmial.com");
+var_dump($arrayusuarios);
